59d2e17dc27dd23b5d1f4a05e0ae128a9c63095a
Amar Abbas <amarabbas1414@gmail.com>
2025-08-26 16:13:19 +0500

    add headers becuase site blockiing my request

--- worldmeters/spiders/countries.py
+++ worldmeters/spiders/countries.py
@@ -5,15 +5,24 @@ class CountriesSpider(scrapy.Spider):
     allowed_domains = ['worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url,
+                headers={
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                                  "Chrome/115.0 Safari/537.36"
+                }
+            )
+
     def parse(self, response):
         #using xpath method
-      #  title=response.xpath("//h1/text()").get()
-        countries = response.xpath("//td/a").getall()
-        for country in countries:
-            name = country.xpath(".//text()")
-            link = country.xpath(".//@href()")
-            yield{
-                'country_name':name,
-                'country_link':link
-            }
+        title=response.xpath("//h1/text()").get()
+        countries = response.xpath("//td/a/text()").getall()
+
+        yield{
+            'title':title,
+            'countries':countries
+        }
         
