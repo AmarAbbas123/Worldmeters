41be4815fa0fd2a91f4deda46632f8a2c84323b6
Amar Abbas <amarabbas1414@gmail.com>
2025-08-27 01:22:14 +0500

    scrap all country data

--- worldmeters/spiders/countries.py
+++ worldmeters/spiders/countries.py
@@ -27,14 +27,46 @@ class CountriesSpider(scrapy.Spider):
            # absolute_url = f"https://www.worldometers.info/{link}"
            # absolute_url = response.urljoin(link)
            # yield scrapy.Request(url=absolute_url)
-            yield response.follow(url=link, headers=self.custom_headers,callback=self.parse_country)
+            yield response.follow(
+                url=link,
+                headers=self.custom_headers,
+                callback=self.parse_country,
+                meta={'country': name}
+            )
+
         
     def parse_country(self,response):
+        country = response.meta['country']
         rows = response.xpath("(//table[contains(@class,'datatable')])[1]/tbody/tr")
         for row in rows:
             year = row.xpath(".//td[1]/text()").get()
             population = row.xpath(".//td[2]/text()").get()
+            Yearly_percent_Change = row.xpath(".//td[3]/text()").get()
+            Yearly_Change = row.xpath(".//td[4]/text()").get()
+            Migrants = row.xpath(".//td[5]/text()").get()
+            Median = row.xpath(".//td[6]/text()").get()
+            Fertility = row.xpath(".//td[7]/text()").get()
+            Density = row.xpath(".//td[8]/text()").get()
+            Urban_Pop  = row.xpath(".//td[9]/text()").get()
+            Urban_Population = row.xpath(".//td[10]/text()").get()
+            Country_Share = row.xpath(".//td[11]/text()").get()
+            World_Population = row.xpath(".//td[12]/text()").get()
+            Pakistan_Global_Rank = row.xpath(".//td[13]/text()").get()
+
         yield {
+            'country': country,
             'year' : year,
-            'population' : population
+            'population' : population,
+            'Yearly_%_Change': Yearly_percent_Change,
+            'Yearly_Change'  : Yearly_Change,
+            "Migrants_(net)" : Migrants,
+            "Median Age" : Median,
+            "Fertility Rate" :Fertility,
+            "Density (P/Km²)" : Density,
+            "Urban_Pop %": Urban_Pop,
+            "Urban Population" : Urban_Population,
+            "Country's Share of World Pop" : Country_Share,
+            "World_Population" : World_Population,
+            "Pakistan Global Rank" : Pakistan_Global_Rank
+
         }   
