24da231d60b248f09c9041ca0903e2bf86284e27
Amar Abbas <amarabbas1414@gmail.com>
2025-08-27 00:57:05 +0500

    fix 403 error by adding custom headers for every request

--- worldmeters/spiders/countries.py
+++ worldmeters/spiders/countries.py
@@ -5,17 +5,17 @@ class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
-
+    custom_headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/115.0 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Referer": "https://www.google.com/",
+    }
+    
     def start_requests(self):
         for url in self.start_urls:
-            yield scrapy.Request(
-                url,
-                headers={
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                                  "AppleWebKit/537.36 (KHTML, like Gecko) "
-                                  "Chrome/115.0 Safari/537.36"
-                }
-            )
+            yield scrapy.Request(url, headers=self.custom_headers)
 
     def parse(self, response):
         #using xpath method
@@ -27,10 +27,10 @@ class CountriesSpider(scrapy.Spider):
            # absolute_url = f"https://www.worldometers.info/{link}"
            # absolute_url = response.urljoin(link)
            # yield scrapy.Request(url=absolute_url)
-            yield response.follow(url=link, callback=self.parse_country)
+            yield response.follow(url=link, headers=self.custom_headers,callback=self.parse_country)
         
     def parse_country(self,response):
-        rows=response.xpath("(//table[@class='datatable w-full border border-zinc-200 datatable-table'])[1]/tbody/tr")
+        rows = response.xpath("(//table[contains(@class,'datatable')])[1]/tbody/tr")
         for row in rows:
             year = row.xpath(".//td[1]/text()").get()
             population = row.xpath(".//td[2]/text()").get()
